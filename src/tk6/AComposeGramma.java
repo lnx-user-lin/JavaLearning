6bf7f205bfd9873e5ecb6e63255658daf892ba95
lnx-user-lin <plasma4geek@gmail.com>
2018-12-04 22:13:48 +0800

    new

--- src/tk6/AComposeGramma.java
+++ src/tk6/AComposeGramma.java
@@ -6,6 +6,9 @@ class WaterFall{
 		System.out.println("construtor");
 		water="constructor";
 	}
+	public String testNullClass() {
+		return "methoud can use";
+	}
 	public String toString() {
 		return water;
 	}
@@ -18,8 +21,15 @@ class WaterFall{
  *
  */
 public class AComposeGramma {
-	private String valve1,valve2,valve3,valve4,valve5;
-	private WaterFall w3=new WaterFall();
+	private String valve1,valve2,valve3,valve4,valve5;		
+	
+	//
+	private WaterFall w3=new WaterFall();	
+	/*若引用的对象waterfall未初始化，
+	则自动初始化为null，
+	调用waterfall的方法或成员时出错*/
+	
+	private WaterFall w4;
 	private int i;
 	private float f;
 	/**
@@ -41,6 +51,16 @@ public class AComposeGramma {
 		System.out.println(acg.w3);
 		AComposeGramma bgc;
 		//System.out.println(bgc);//bgc未初始化，无法调用内部数据或方法
+		AComposeGramma dgc=new AComposeGramma();
+		/*
+		 * 虽然w4未初始化，但仍然可以使用tostring并返回一个null
+		 * */
+		System.out.println(dgc.w4);
+		/*
+		 * w4未初始化不能使用方法testNullClass
+		 * */
+		System.out.println(acg.w4.testNullClass());
+		System.out.println(acg.w3.testNullClass());
 	}
 
 }
